246456b3ca09be1f69353fa18518aaea8e8e51c7
Jas10022 <jas10022jb@gmail.com>
2019-07-20 16:09:27 -0700

    Database connections

--- app/src/main/java/com/example/jas10022/parkingapp/MainActivity.java
+++ app/src/main/java/com/example/jas10022/parkingapp/MainActivity.java
@@ -1,26 +1,40 @@
 package com.example.jas10022.parkingapp;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+
+import android.location.LocationListener;
+
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.here.android.mpa.common.GeoCoordinate;
 import com.here.android.mpa.common.OnEngineInitListener;
 import com.here.android.mpa.mapping.Map;
 import com.here.android.mpa.mapping.MapMarker;
 import com.here.android.mpa.mapping.SupportMapFragment;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity{
 
     //This is the refrence of the Firebase Stoarage
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 
     @Override
@@ -31,6 +45,9 @@ public class MainActivity extends AppCompatActivity {
         final SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapfragment);
         // initialize the Map Fragment and
         // retrieve the map that is associated to the fragment
+
+        //jas
+
         mapFragment.init(new OnEngineInitListener() {
             @Override
             public void onEngineInitializationCompleted(
@@ -39,11 +56,14 @@ public class MainActivity extends AppCompatActivity {
                     // now the map is ready to be used
                     final Map map = mapFragment.getMap();
 
+                    map.setCenter(new GeoCoordinate(37.787, -121.396 , 0.0), Map.Animation.NONE);
+                    map.setZoomLevel((map.getMaxZoomLevel() + map.getMinZoomLevel()) / 2);
+
                     //create a geoCordinate based off the long and latitude
                     //this is how you cna create a new Map Marker in a specified location
                     //map.addMapObject(new MapMarker(new GeoCoordinate(49.163, -123.137766, 10)));
 
-
+                    //This is how to get from the realt time database
                     myRef.child("0").addValueEventListener(new ValueEventListener() {
                         @Override
                         public void onDataChange(DataSnapshot dataSnapshot) {
@@ -64,6 +84,28 @@ public class MainActivity extends AppCompatActivity {
                             Log.w("MainActivity", "Failed to read value.", error.toException());
                         }
                     });
+
+                    //jas's code- this is how you get it from the firestore database
+                    db.collection("Ratings").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                        @Override
+                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                            for (DocumentSnapshot document: queryDocumentSnapshots.getDocuments()){
+
+                                System.out.println(document.getId() + "=>" + document.getData());
+                                double Ratings = (double)document.get("Rating");
+                                int numOfRatings = (int)document.get("Number of Ratings");
+                                GeoPoint location = (GeoPoint) document.get("Location");
+                                Coordinate loc = new Coordinate(location.getLatitude(),location.getLongitude());
+                                ParkingLocation parkingLocation = new ParkingLocation(loc, Ratings,numOfRatings);
+
+
+                                //this is where you want to populate the local hash map
+
+                            }
+                        }
+                    });
+
+
 
                     /* This is how you can retrive a value
                     myRef.addValueEventListener(new ValueEventListener() {
