e82806f3def35c163f9fe3763ffc28698aa04781
Saketh Kollu <saketh.kollu@gmail.com>
2019-07-20 20:13:42 -0700

    updated api

--- app/src/main/java/com/example/jas10022/parkingapp/ParkingLot.java
+++ app/src/main/java/com/example/jas10022/parkingapp/ParkingLot.java
@@ -4,17 +4,20 @@ import java.util.Set;
 
 public class ParkingLot extends ParkingLocation {
 
+    private Coordinate location;
     private long maxCapacity;
     private long currentCapacity;
 
     public ParkingLot(Coordinate location, long maxCapacity, long currentCapacity) {
         super(location);
+        this.location = this.getLocation();
         this.maxCapacity = maxCapacity;
         this.currentCapacity = currentCapacity;
     }
 
     public ParkingLot(Coordinate location, double rating, int num_ratings, long maxCapacity, long currentCapacity) {
         super(location, rating, num_ratings);
+        this.location = this.getLocation();
         this.maxCapacity = maxCapacity;
         this.currentCapacity = currentCapacity;
     }
