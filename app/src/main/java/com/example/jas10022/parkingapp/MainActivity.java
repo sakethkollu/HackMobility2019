0d10d7d960b4a846f6d51d16edf624ebf1db7efb
Jas10022 <jas10022jb@gmail.com>
2019-07-21 01:19:41 -0700

    UI fixes

--- app/src/main/java/com/example/jas10022/parkingapp/MainActivity.java
+++ app/src/main/java/com/example/jas10022/parkingapp/MainActivity.java
@@ -22,6 +22,7 @@ import android.view.Display;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.RatingBar;
@@ -82,7 +83,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
     boolean click = true;
     private PopupWindow currentWindow;
     public static KDTree parkingCoordinates;
-    public FloatingActionButton currentLocation;
+    public ImageButton currentLocation;
     public FloatingActionButton goToDirections;
     private GeoCoordinate currentMarker;
     private RelativeLayout mainLayout;
@@ -102,7 +103,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
         height = size.y;
         // initialize the Map Fragment and
         // retrieve the map that is associated to the fragment
-        currentLocation = findViewById(R.id.curent_location);
+        currentLocation = findViewById(R.id.current_location);
         goToDirections = findViewById(R.id.go_button);
         goToDirections.hide();
         //mainLayout.setBackground(getResources().getDrawable(R.drawable.background));
@@ -222,7 +223,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
                                                             currentWindow.showAtLocation(new LinearLayout(getBaseContext()), Gravity.BOTTOM, width / 50, height / 30);
                                                             //popUp.update(50, 50, 300, 80);
                                                         goToDirections.show();
-                                                        currentLocation.hide();
+                                                        currentLocation.setVisibility(View.GONE);
                                                             click = false;
 
                                                         System.out.println("selected location: " + currentMarker.getLatitude() + " : " + currentMarker.getLongitude());
@@ -234,7 +235,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
                                         currentWindow.dismiss();
                                         click = true;
                                         goToDirections.hide();
-                                        currentLocation.show();
+                                        currentLocation.setVisibility(View.VISIBLE);
                                     }
                                     return false;
 
